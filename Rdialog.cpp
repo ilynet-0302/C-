fa674c974458fd4aff712f1a50e7b5bcad108d0a
ilynet-0302 <43601053+ilynet-0302@users.noreply.github.com>
2018-10-03 11:32:19 +0300

    asd
    asd

---
+++ Rdialog.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Kak se kazvash?";
+    char ime[15];
+    cin>>ime;
+    cout<<"Zdravei "<<ime<<"!"<<endl;
+    cout<<"Na kolko godini si?";
+    char godini[2];
+    cin>>godini;
+    cout<<"I az sum na "<<godini<<"!"<<endl;
+    return 0;
+}
