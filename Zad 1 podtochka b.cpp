1efe0126879b59313d9c862f69c20a12f3edc82b
ilynet-0302 <43601053+ilynet-0302@users.noreply.github.com>
2018-11-12 11:31:37 +0200

    Test 11.12.2018
    Test

---
+++ Zad 1 podtochka b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,a,b,c,d;
+    cin>>x;
+    a=x/1000;
+    b=x/100%10;
+    c=x/10%10;
+    d=x%10;
+    cout<<b<<c<<d<<a<<endl;
+    return 0;
+
+}
